ddf29ebd735f92705d16106eb6b6da68fb0bc4cc
Mason Wendel <96151778+MasonWendel@users.noreply.github.com>
2023-09-20 20:37:59 +0000

    Did Artur

--- invitational-a-2022-MasonWendel-main/invitational-a-2022-MasonWendel-main/Artur.java
+++ invitational-a-2022-MasonWendel-main/invitational-a-2022-MasonWendel-main/Artur.java
@@ -1,9 +1,27 @@
 public class Artur{
-    public satic void main(String[] args){
+    public static void main(String[] args){
         int lineDif = 2; 
-        for(int i = 1; i<=20){
-            int filling = 20-i; 
-            System.out.print(filling+)
+        int count = 0; 
+        double num = 0.1; 
+        String filling = "";
+        for(int i = 0; i<20; i++){
+            filling+=" ";
+        }
+        for(int i = 0; i<20;i++){
+            int fillingNum = 20-i; 
+            String filling2  = "";
+            for(int j  = 0 ; j<count; j++){
+                filling2+="  ";
+            }
+            System.out.print(filling.substring(0,fillingNum)+"/"+filling2+"\\");
+            if(count%4==0){
+                num/=10;
+                
+                System.out.printf("--->%."+((count/4)+2)+"f",num);
+                System.out.print("%");
+            }
+            count++;
+            System.out.print("\n");
         }
     }
 }
