52e721616c299726e58683432f6fca7f9f59a469
Mason Wendel <Masonwendel@outlook.com>
2023-09-20 13:28:00 -0500

    Started artur

---
+++ invitational-a-2022-MasonWendel-main/invitational-a-2022-MasonWendel-main/Artur.java
@@ -0,0 +1,9 @@
+public class Artur{
+    public satic void main(String[] args){
+        int lineDif = 2; 
+        for(int i = 1; i<=20){
+            int filling = 20-i; 
+            System.out.print(filling+)
+        }
+    }
+}
